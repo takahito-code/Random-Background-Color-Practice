8de149092a0aa798f59a187520c9e865e3c61dd0
Takahito Yokouchi <taka@Takahitos-MacBook-Air.local>
2025-01-12 16:42:09 +0900

    Update

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const button = document.getElementById("changeColorButton");
-
+const colorCodeDisplay = document.getElementById("colorCode");
 
 button.addEventListener("click", () => {
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     document.body.style.backgroundColor = randomColor;
     
-    const colorCodeDisplay = document.getElementById("colorCode");
+    
     colorCodeDisplay.textContent = `Current Color: ${randomColor}`;
 });
 
-
+colorCodeDisplay.addEventListener("click", ()=> {
+const color = colorCodeDisplay.textContent.replace("Current Color: ", "");
+navigator.clipboard.writeText(color).then(() => {
+    alert (`copied to clipboard: ${color}`);
+}).catch(()=> {
+    alert("Failed to copy the color code.");
+})
+});
