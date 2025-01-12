7edc43745b708ad1563f9b0b8a384c2993a3978e
Takahito Yokouchi <taka@Takahitos-MacBook-Air.local>
2025-01-12 16:22:44 +0900

    Update

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const button = document.getElementById("changeColorButton");
 
+
 button.addEventListener("click", () => {
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     document.body.style.backgroundColor = randomColor;
+    
+    const colorCodeDisplay = document.getElementById("colorCode");
+    colorCodeDisplay.textContent = `Current Color: ${randomColor}`;
 });
+
+
